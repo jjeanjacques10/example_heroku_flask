4575ff364e89dd2c6119ced32c5a4443c6984748
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-06-13 02:10:50 -0300

    Fisrt commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_cors import CORS 
+
+app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>Hello World!</h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    main()
